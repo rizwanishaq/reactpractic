7069f1af495d9de6e5958fd64c5088a5d1a84250
rizwanishaq <rizwanishaq@gmail.com>
2019-12-20 11:52:47 +0100

    Updates

---
+++ precache-manifest.1c88facd36db3cc3091b7052480ff14d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed6d0ccb6756f4a91ce4688af0eef765",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe3cdf2f7317fe640420",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "1ff5c5ca1ad181ab6a88",
+    "url": "/static/js/2.b709e730.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.b709e730.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fe3cdf2f7317fe640420",
+    "url": "/static/js/main.01338770.chunk.js"
+  },
+  {
+    "revision": "a26b79035772aebf505b",
+    "url": "/static/js/runtime-main.53d0af3d.js"
+  },
+  {
+    "revision": "84d6226f0e3137748debbbe77cbec922",
+    "url": "/static/media/oval.84d6226f.svg"
+  }
+]);
